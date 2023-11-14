afab02ce9e9c0a9d6a06c16287aea3ad7b06312d
Mauro Arantes <mauro.arantes@gmail.com>
2023-11-14 12:41:51 +0000

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Mauro Arantes on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
